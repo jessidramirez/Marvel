c826877446064b4a08c9b0a1ef0f86a80f1c27a8
Jessid Ramirez <jessid.alexander@gmail.com>
2025-04-06 21:56:55 -0500

    Proyecto Version 1.1

--- app/src/main/java/com/example/marvel/FotosFragment.kt
+++ app/src/main/java/com/example/marvel/FotosFragment.kt
@@ -31,7 +31,6 @@ class FotosFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
     }
 
     override fun onDestroyView() {
